a680d8f4f9a73644a29b82647e72a9fdea86ee4f
HsuHsia <807817697@qq.com>
2021-06-29 12:59:35 +0800

    首页做了一半了

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComp/HomeSwiper";
+import RecommendView from "./childComp/RecommendView";
+import { getHomeMultidata } from "network/home";
+export default {
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  // 组件创建完成后立即请求数据
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.data.banner.list;
+      this.recommends = res.data.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style>
+.home-nav {
+  color: #fff;
+  background-color: var(--color-tint);
+}
 </style>
